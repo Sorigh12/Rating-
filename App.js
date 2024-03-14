086e7efd1803ea6ad09a8571d2e9026719be165b
Sorigh <84138511+Sorigh12@users.noreply.github.com>
2024-03-14 06:20:37 -0400

    Create App.js

---
+++ App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import StarRating from './StarRating';
+
+function App() {
+  return (
+    <div className="App">
+   
+      <StarRating/>
+          
+
+
+      </div>
+   
+  );
+}
+
+export default App;
